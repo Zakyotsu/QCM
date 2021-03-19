dec8498202eee390f7294f700c0b59db64a24256
unknown <47122315+Zakyotsu@users.noreply.github.com>
2021-03-19 10:02:32 +0100

    Lib

--- DAO.java
+++ DAO.java
@@ -1,6 +1,40 @@
 package fr.vinet.qcm;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class DAO {
-	private String nomFichier;
-	
+	private File csv = new File("qcm.csv");
+
+	public ArrayList<Question> getQuestions() {
+		String line = "";
+		ArrayList<Question> questions = new ArrayList<>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(csv));
+			while ((line = br.readLine()) != null) {
+				String[] splitted = line.split(";");
+				for(String s : line.split(";")) System.out.println(s);
+				String theme = splitted[0];
+				String question = splitted[1];
+
+				String[] leurre = new String[4];
+				leurre[0] = splitted[2];
+				leurre[1] = splitted[3];
+				leurre[2] = splitted[4];
+				leurre[3] = splitted[5];
+
+				String answer = splitted[6];
+				String comment = splitted[7];
+
+				questions.add(new Question(question, theme, leurre, answer, comment));
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return questions;
+	}
 }
--- Jeu.java
+++ Jeu.java
@@ -2,4 +2,12 @@ package fr.vinet.qcm;
 
 public class Jeu {
 
+    public static void main(String[] args) {
+        DAO dao = new DAO();
+
+        for(Question q : dao.getQuestions()) {
+            System.out.println(q.getQuestion());
+        }
+    }
+
 }
